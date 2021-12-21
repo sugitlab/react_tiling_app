e63797cfc08e95f2beffb588a930ad44a4b547d8
Sugit <sgmt.snj@gmail.com>
2021-12-22 00:38:03 +0900

    feat: react-mosaic-component simple app

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { DndProvider } from "react-dnd";
+import { HTML5Backend } from "react-dnd-html5-backend";
+import { Mosaic, MosaicWindow } from "react-mosaic-component";
+import "react-mosaic-component/react-mosaic-component.css";
+import "./App.css";
 
-function App() {
+export type ViewId = "a" | "b" | "c" | "new";
+
+const TITLE_MAP: Record<ViewId, string> = {
+  a: "Left Window",
+  b: "Top Right Window",
+  c: "Bottom Right Window",
+  new: "New Window",
+};
+
+const Mosaic2 = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <DndProvider backend={HTML5Backend}>
+      <Mosaic<ViewId>
+        renderTile={(id, path) => (
+          <MosaicWindow<ViewId>
+            path={path}
+            createNode={() => "new"}
+            title={TITLE_MAP[id]}
+          >
+            <h1>{TITLE_MAP[id]}</h1>
+          </MosaicWindow>
+        )}
+        initialValue={{
+          direction: "row",
+          first: "a",
+          second: {
+            direction: "column",
+            first: "b",
+            second: "c",
+          },
+        }}
+      />
+    </DndProvider>
+  );
+};
+
+const ELEMENT_MAP: { [viewId: string]: JSX.Element } = {
+  a: <div>Left Window</div>,
+  b: <div>Top Right Window</div>,
+  c: <div>Bottom Right Window</div>,
+};
+
+const Mosaic1 = () => {
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <div id="app">
+        <Mosaic<string>
+          renderTile={(id) => ELEMENT_MAP[id]}
+          initialValue={{
+            direction: "row",
+            first: "a",
+            second: {
+              direction: "column",
+              first: "b",
+              second: "c",
+            },
+            splitPercentage: 40,
+          }}
+        />
+      </div>
+    </DndProvider>
+  );
+};
+
+const App = () => {
+  return (
+    <div style={{ height: "800px" }}>
+      <Mosaic2 />
     </div>
   );
-}
-
+};
 export default App;
